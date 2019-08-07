98762a063e4bd7b7eaa79cdbbd3234c71d47429a
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-08-07 12:58:11 -0700

    added styling

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -16,56 +16,59 @@ const UserForm = ({ errors, touched, values, handleSubmit, status }) => {
         }, [status]);
   
     return (
-      <div className="user-form">
-        <h1>User Onboarding Form</h1>
-        <Form>
-          <Field type="text" name="name" placeholder="Name" />
-          {touched.name && errors.name && (
-            <p className="error">{errors.name}</p>
-          )}
-  
-          <Field type="text" name="email" placeholder="Email" />
-          {touched.email && errors.email && <p className="error">{errors.email}</p>}
+        <div className = 'container'>
+            <div className="user-form">
+                        <h1>User Onboarding Form</h1>
+                        <Form>
+                        <Field type="text" name="name" placeholder="Name" />
+                        {touched.name && errors.name && (
+                            <p className="error">{errors.name}</p>
+                        )}
+                
+                        <Field type="text" name="email" placeholder="Email" />
+                        {touched.email && errors.email && <p className="error">{errors.email}</p>}
 
-          <Field type="text" name="password" placeholder="Password" />
-          {touched.password && errors.password && <p className="error">{errors.password}</p>}
+                        <Field type="text" name="password" placeholder="Password" />
+                        {touched.password && errors.password && <p className="error">{errors.password}</p>}
 
-          <Field component="select" className="role-select" name="role">
-          <option>Please Choose an Option</option>
-          <option value="Front-end developer">Front-end developer</option>
-          <option value="Back-end developer">Back-end developer</option>
-          <option value="UX designer">UX designer</option>
-          <option value="UI designer">UI designer</option>
-          <option value="Data scientist">Data scientist</option>
-        </Field>
-  
-          <label className="checkbox-container">
-            Terms of Service
-            <Field
-              type="checkbox"
-              name="terms"
-              checked={values.terms}
-            />
-            <span className="checkmark" />
-          </label>
-  
-          <button type="submit">Submit!</button>
-        </Form>
-
-        {users.map(user => (
-        <Card>
-            <div className = "ui cards">
-                <div class="card">
-                    <div class="content">
-                        <div class="header" key={user.id}>{user.name}</div>
-                        <div class="meta" key={user.id}>{user.role}</div>
+                        <Field component="select" className="role-select" name="role">
+                        <option>Please Choose an Option</option>
+                        <option value="Front-end developer">Front-end developer</option>
+                        <option value="Back-end developer">Back-end developer</option>
+                        <option value="UX designer">UX designer</option>
+                        <option value="UI designer">UI designer</option>
+                        <option value="Data scientist">Data scientist</option>
+                        </Field>
+                
+                        <label className="checkbox-container">
+                            Terms of Service
+                            <Field
+                            type="checkbox"
+                            name="terms"
+                            checked={values.terms}
+                            />
+                            <span className="checkmark" />
+                        </label>
+            
+                        <button type="submit">Submit!</button>
+                        </Form>
                     </div>
-                </div>
-            </div>
-        </Card>
-      ))}
 
-      </div>
+            <div className='user-list'>
+                {users.map(user => (
+                    <Card>
+                        <div className = "ui raised cards">
+                            <div class="card">
+                                <div class="content">
+                                    <div class="header" key={user.id}>{user.name}</div>
+                                    <div class="meta" key={user.id}>{user.role}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </Card>
+                ))}
+            </div>
+        </div>
     );
   };
   
